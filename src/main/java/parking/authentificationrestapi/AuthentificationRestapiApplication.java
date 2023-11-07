ce4b5e6a10a79f6640a7a41c469538abcc3588c5
Coretchi Tudor <tudorcoretchi119@gmail.com>
2023-11-07 18:34:54 +0200

    Initial commit

---
+++ src/main/java/parking/authentificationrestapi/AuthentificationRestapiApplication.java
@@ -0,0 +1,13 @@
+package parking.authentificationrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthentificationRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/parking/authentificationrestapi/AuthentificationRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package parking.authentificationrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
